22325feb830b0d0fc33429537708cb47bc3cfd9a
CvetelinStoimenov <stoimenovv@gmail.com>
2024-07-22 21:12:57 +0300

    22.07.24

--- 06.Exercise Selenium WebDriver Wait/ExerciseSeleniumWaits/4.WorkingWithAlerts/WorkingWithAlerts.cs
+++ 06.Exercise Selenium WebDriver Wait/ExerciseSeleniumWaits/4.WorkingWithAlerts/WorkingWithAlerts.cs
@@ -26,6 +26,38 @@ namespace _4.WorkingWithAlerts
         public void HandlingBasicAlerts()
         {
             driver.FindElement(By.XPath("//*[@id='content']/div/ul/li[1]/button")).Click();
+            driver.SwitchTo().Alert().Accept();
+            IWebElement result = driver.FindElement(By.XPath("//*[@id='result']"));
+            Assert.That(result.Text, Is.EqualTo("You successfully clicked an alert"), "The alert massage is not closed.");
+        }
+
+        [Test]
+        public void HandlingConfirmationAlertsAccept()
+        {
+            driver.FindElement(By.XPath("//*[@id='content']/div/ul/li[2]/button")).Click();
+            driver.SwitchTo().Alert().Accept();
+            IWebElement result = driver.FindElement(By.XPath("//*[@id='result']"));
+            Assert.That(result.Text, Is.EqualTo("You clicked: Ok"), "The alert massage is not closed.");
+        }
+
+        [Test]
+        public void HandlingConfirmationAlertsDismiss()
+        {
+            driver.FindElement(By.XPath("//*[@id='content']/div/ul/li[2]/button")).Click();
+            driver.SwitchTo().Alert().Dismiss();
+            IWebElement result = driver.FindElement(By.XPath("//*[@id='result']"));
+            Assert.That(result.Text, Is.EqualTo("You clicked: Cancel"), "The alert massage is not closed.");
+        }
+
+        [Test]
+        public void HandlingPromptAlerts()
+        {
+            driver.FindElement(By.XPath("//*[@id='content']/div/ul/li[3]/button")).Click();
+            IAlert alert = driver.SwitchTo().Alert();
+            alert.SendKeys("I will learn Selenium!!!");
+            alert.Accept();
+            IWebElement result = driver.FindElement(By.XPath("//*[@id='result']"));
+            Assert.That(result.Text, Is.EqualTo("You entered: I will learn Selenium!!!"), "The alert massage is not closed.");
         }
     }
 }
--- 06.Exercise Selenium WebDriver Wait/ExerciseSeleniumWaits/5.WorkingWithIFrames/WorkingWithIFrames.cs
+++ 06.Exercise Selenium WebDriver Wait/ExerciseSeleniumWaits/5.WorkingWithIFrames/WorkingWithIFrames.cs
@@ -1,16 +1,98 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
 namespace _5.WorkingWithIFrames
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
+            driver = new ChromeDriver();
+            driver.Url = "https://codepen.io/pervillalva/full/abPoNLd";
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
         }
 
         [Test]
-        public void Test1()
+        public void HandlingIFramesByIndex()
         {
-            Assert.Pass();
+            // Wait until iFrame is available and switch to it by finding the firs iFrame
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.TagName("iframe")));
+
+            // click the dropdown button
+            var dropdownButton = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".dropbtn")));
+            dropdownButton.Click();
+
+            // select the link inside the dropdown menu
+            var dropdownLinks = wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector(".dropdown-content a")));
+
+            // Verify and print the link text
+            foreach (var link in dropdownLinks)
+            {
+                Console.WriteLine(link.Text);
+                Assert.IsTrue(link.Displayed, "Link inside the dropdown is nor displayed as expected");
+            }
+            driver.SwitchTo().DefaultContent();
+        }
+
+        [Test]
+        public void HandlingIFramesById()
+        {
+            // Wait until iFrame is available and switch to it by id
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt("result"));
+
+            // click the dropdown button
+            var dropdownButton = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".dropbtn")));
+            dropdownButton.Click();
+
+            // select the link inside the dropdown menu
+            var dropdownLinks = wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector(".dropdown-content a")));
+
+            // Verify and print the link text
+            foreach (var link in dropdownLinks)
+            {
+                Console.WriteLine(link.Text);
+                Assert.IsTrue(link.Displayed, "Link inside the dropdown is nor displayed as expected");
+            }
+            driver.SwitchTo().DefaultContent();
+        }
+
+        [Test]
+        public void HandlingIFramesByWebElement()
+        {
+            // Locate the frame element
+            var frameElement = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#result")));
+
+            // Switch to the frame by web element
+            driver.SwitchTo().Frame(frameElement);
+
+            // click the dropdown button
+            var dropdownButton = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".dropbtn")));
+            dropdownButton.Click();
+
+            // select the link inside the dropdown menu
+            var dropdownLinks = wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.CssSelector(".dropdown-content a")));
+
+            // Verify and print the link text
+            foreach (var link in dropdownLinks)
+            {
+                Console.WriteLine(link.Text);
+                Assert.IsTrue(link.Displayed, "Link inside the dropdown is nor displayed as expected");
+            }
+            driver.SwitchTo().DefaultContent();
         }
     }
 }
